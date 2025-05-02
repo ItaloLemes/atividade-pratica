ae645dcd0d5dcc86ea58a0f5d1a4801095b9ed27
ItaloLemes <160165023+ItaloLemes@users.noreply.github.com>
2025-05-02 09:34:28 -0300

    Update Fachada.java

--- geometria_projeto/src/back/negocio/Fachada.java
+++ geometria_projeto/src/back/negocio/Fachada.java
@@ -6,19 +6,16 @@ import back.dado.Triangulo;
 
 public class Fachada implements iFachada {
 
-    @Override
     public double calcularAreaCirculo(double raio) {
         Circulo c = new Circulo(raio);
         return c.calcularArea();
     }
 
-    @Override
     public double calcularAreaRetangulo(double largura, double altura) {
         Retangulo r = new Retangulo(largura, altura);
         return r.calcularArea();
     }
 
-    @Override
     public double calcularAreaTriangulo(double base, double altura) {
         Triangulo t = new Triangulo(base, altura);
         return t.calcularArea();
